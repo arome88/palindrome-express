3ac46a58f60e9af368e58267c98932f17ad3e826
arome88 <anaromero.88@icloud.com>
2023-04-30 02:23:05 -0400

    first commit

--- server.js
+++ server.js
@@ -2,75 +2,71 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
+app.use('/public', express.static('public'));
 
 var db, collection;
 
-const url = "mongodb+srv://demo:demo@cluster0-q2ojb.mongodb.net/test?retryWrites=true";
-const dbName = "demo";
+const url = "mongodb+srv://anaromero88:Nmt9jtnG5WVaXCVY@cluster0.v1trw0i.mongodb.net/";
+const dbName = 'palindrome'
 
-app.listen(3000, () => {
-    MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
-        if(error) {
-            throw error;
-        }
-        db = client.db(dbName);
-        console.log("Connected to `" + dbName + "`!");
+app.listen(2000, function () {
+  MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true },
+    (error, client) => {
+      if (error) {
+        throw error;
+      }
+      db = client.db(dbName);
+      console.log("Connected to `" + dbName + "`!");
     });
+
 });
 
 app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  db.collection('messages').find().toArray((err, result) => {
+  db.collection('palindrome')
+  .find()
+  .toArray((err, result) => {
     if (err) return console.log(err)
-    res.render('index.ejs', {messages: result})
+    res.render('index.ejs', { palindrome: result })
   })
+
 })
 
-app.post('/messages', (req, res) => {
-  db.collection('messages').insertOne({name: req.body.name, msg: req.body.msg, thumbUp: 0}, (err, result) => {
-    if (err) return console.log(err)
-    console.log('saved to database')
-    res.redirect('/')
-  })
+app.post('/userWord', (req, res) => {
+  let word = `${req.body.word}`
+  let findPalindrome = word.toLowerCase().split('').reverse().join('')
+
+  if (word.toLowerCase() === findPalindrome) {
+    palindromeResult = 'is a palindrome';
+  } else {
+    palindromeResult = 'can not be spelled the same backwards'
+  }
+
+  db.collection('palindrome').insertOne(
+    {
+      word: req.body.word,
+      outcome: palindromeResult
+    })
+    .then(result => {
+      console.log('saved to database')
+      res.redirect('/')
+    })
+    .catch(error => console.error(error))
 })
 
-app.put('/messages', (req, res) => {
-  db.collection('messages')
-  .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
-    $set: {
-      thumbUp:req.body.thumbUp + 1
-    }
-  },  {
-    sort: {_id: -1},
-    upsert: true
-  }, (err, result) => {
-    if (err) return res.send(err)
-    res.send(result)
-  })
-})
 
-app.put('/messages/thumbDown', (req, res) => {
-  db.collection('messages')
-  .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
-    $set: {
-      thumbUp:req.body.thumbUp - 1
-    }
-  },  {
-    sort: {_id: -1},
-    upsert: true
-  }, (err, result) => {
-    if (err) return res.send(err)
-    res.send(result)
-  })
-})
 
-app.delete('/messages', (req, res) => {
-  db.collection('messages').findOneAndDelete({name: req.body.name, msg: req.body.msg}, (err, result) => {
-    if (err) return res.send(500, err)
-    res.send('Message deleted!')
+app.delete('/deleteWord', (req, res) => {
+  db.collection('palindrome').findOneAndDelete(
+    {
+      word: req.body.word
+    },
+    (err, result) => {
+      if (err) return res.send(500, err)
+      res.send('Message deleted!')
   })
 })
